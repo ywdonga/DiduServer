48e68ff87d91c592d9e8bcd51d835d9c09979022
dongyouwei <dongyouweie@126.com>
2023-03-10 18:17:19 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -11,7 +11,8 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0")
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0")
     ],
     targets: [
         .target(
@@ -20,7 +21,8 @@ let package = Package(
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
                 .product(name: "Leaf", package: "leaf"),
-                .product(name: "Vapor", package: "vapor")
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "JWTKit", package: "jwt-kit")
             ],
             swiftSettings: [
                 // Enable better optimizations when building in Release configuration. Despite the use of
--- Sources/App/Controllers/User/UserController.swift
+++ Sources/App/Controllers/User/UserController.swift
@@ -8,47 +8,50 @@
 import Vapor
 
 struct UserController: RouteCollection {
-    func boot(routes: RoutesBuilder) throws {
-        let users = routes.grouped("users")
-        users.post("register", use: register)
-        users.post("login", use: login)
+    
+    func boot(routes: Vapor.RoutesBuilder) throws {
+        
     }
     
-    func register(req: Request) throws -> EventLoopFuture<User> {
-        try User.Create.validate(content: req)
-        let userCreate = try req.content.decode(User.Create.self)
-        let user = try User(email: userCreate.email, password: Bcrypt.hash(userCreate.password))
-        return user.save(on: req.db).map { user }
-    }
-    
-    func login(req: Request) throws -> EventLoopFuture<HTTPStatus> {
-        let loginData = try req.content.decode(User.Login.self)
+    func validateIdentityTokenHandler(_ req: Request) throws -> EventLoopFuture<User> {
+        let identityToken = try req.content.decode(IdentityToken.self)
+        
+        // Get the JWK Set from Apple's public keys endpoint
+        let jwkSet = try await req.application.apple.verifyJWKSet()
+        
+        // Verify the identity token signature using the JWK Set
+        let signer = JWTSigner.rs256(jwks: jwkSet)
+        let verifiedIdentityToken = try JWT<IdentityTokenPayload>(from: identityToken.token, verifiedUsing: signer)
+        
+        // Verify that the identity token was issued for this app
+        guard verifiedIdentityToken.payload.iss == "https://appleid.apple.com" else {
+            throw Abort(.badRequest, reason: "Invalid issuer")
+        }
+        guard verifiedIdentityToken.payload.aud == "com.yourapp.bundleid" else {
+            throw Abort(.badRequest, reason: "Invalid audience")
+        }
+        
+        // Verify the identity token nonce
+        let nonce = try req.session.get("nonce")
+        guard verifiedIdentityToken.payload.nonce == nonce else {
+            throw Abort(.badRequest, reason: "Invalid nonce")
+        }
+        
+        // Get the user's email address from the identity token
+        let email = verifiedIdentityToken.payload.email
+        
+        // Lookup or create the user based on their email address
         return User.query(on: req.db)
-            .filter(\.$email == loginData.email)
+            .filter(\.$email == email)
             .first()
-            .unwrap(or: Abort(.unauthorized))
-            .flatMapThrowing { user in
-                try Bcrypt.verify(loginData.password, created: user.password)
-            }
-            .flatMap { isAuthenticated in
-                if isAuthenticated {
-                    req.session.data["userId"] = loginData.email
-                    return req.eventLoop.future(.ok)
+            .flatMap { existingUser in
+                if let user = existingUser {
+                    return req.eventLoop.future(user)
                 } else {
-                    return req.eventLoop.future(.unauthorized)
+                    let newUser = User(email: email)
+                    return newUser.save(on: req.db).transform(to: newUser)
                 }
             }
     }
-}
 
-extension User {
-    struct Create: Content {
-        let email: String
-        let password: String
-    }
-    
-    struct Login: Content {
-        let email: String
-        let password: String
-    }
 }
